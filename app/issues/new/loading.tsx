21c76f64385c7fd394669f3df8b3ae659284d6b3
Moncy Yohannan <moncyyohannan12@gmail.com>
2024-09-25 01:17:18 +0530

    Refactor: Organize import statements

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,10 +1,7 @@
+import { Skeleton } from "@/app/components";
 import { Box } from "@radix-ui/themes";
-import delay from "delay";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingNewIssuesPage = async () => {
-  await delay(2000);
   return (
     <Box className="max-w-xl">
       <Skeleton />
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,16 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
-import dynamic from "next/dynamic";
-import "easymde/dist/easymde.min.css";
-import { Controller, useForm } from "react-hook-form";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { issueSchema } from "@/app/validationSchema";
-import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
-import delay from "delay";
+import { issueSchema } from "@/app/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
