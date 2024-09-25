60db98e4beaa83c9c01050ed6b60772161939955
Moncy Yohannan <moncyyohannan12@gmail.com>
2024-09-25 20:02:10 +0530

    build an api for deleting issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -14,7 +14,8 @@ export async function PATCH(
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
-  if (!issue) return NextResponse.json("Invalid Issue.", { status: 404 });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue." }, { status: 404 });
 
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
@@ -25,4 +26,21 @@ export async function PATCH(
     },
   });
   return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue." }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: issue.id },
+  });
+
+  return NextResponse.json({});
 }
