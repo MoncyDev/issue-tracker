d09e71bc7b5a2601191a9eff16a385b6b8943e55
Moncy Yohannan <moncyyohannan12@gmail.com>
2024-09-25 21:11:56 +0530

    Configure  google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,14 @@
 import NextAuth from 'next-auth';
+import GoggleProvider from 'next-auth/providers/google'
 
 const handler= NextAuth({
-    providers: []
-    
+    providers: [
+        GoggleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        })
+    ]
+
 })
 
 export {handler as GET, handler as POST}
