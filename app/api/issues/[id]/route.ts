acbf9ec395b9411b1124b860fb42b76c3a765d9e
Moncy Yohannan <moncyyohannan12@gmail.com>
2024-09-27 01:57:36 +0530

    Enhance the Api to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,6 @@
 import authOptions from "@/app/auth/authOptions";
 import prisma from "@/prisma/client";
-import { issueSchema } from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -9,11 +9,20 @@ export async function PATCH(
   { params }: { params: { id: string } }
 ) {
   const session = await getServerSession(authOptions);
-  if (!session) return NextResponse.json({}, { status: 401 });
+  // if (!session) return NextResponse.json({}, { status: 401 });
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { assignedToUserId, title, description, status } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -23,11 +32,7 @@ export async function PATCH(
 
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: {
-      title: body.title,
-      description: body.description,
-      status: body.status,
-    },
+    data: { title, description, status, assignedToUserId },
   });
   return NextResponse.json(updatedIssue);
 }
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -2,6 +2,21 @@ import { z } from "zod";
 
 export const issueSchema = z.object({
   title: z.string().min(1, "Title is required").max(255),
-  description: z.string().min(1, "Description is required"),
-  status: z.string(),
+  description: z.string().min(1, "Description is required").max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "Assignee is required")
+    .max(255)
+    .optional()
+    .nullable(),
+  status: z.string().min(1, "status is required").max(255).optional(),
 });
