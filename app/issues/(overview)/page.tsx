faaedf108f53903e0a64a578ae12af46986fe28b
Moncy Yohannan <moncyyohannan12@gmail.com>
2024-09-27 06:08:07 +0530

    Sort issues

--- app/issues/(overview)/page.tsx
+++ app/issues/(overview)/page.tsx
@@ -2,22 +2,41 @@ import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "@/app/components";
 import IssueActions from "../IssueActions";
-import { Status } from "@prisma/client";
+import NextLink from "next/link";
+import { Issue, Status } from "@prisma/client";
+import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
 
-interface Props {
-  searchParams: { status: Status };
+export interface searchParams {
+  searchParams: { status: Status; orderBy: keyof Issue; order: string };
 }
 
-const IssuesPage = async ({ searchParams }: Props) => {
+const IssuesPage = async ({ searchParams }: searchParams) => {
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
+
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: searchParams.order || "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
-      status
+      status,
     },
+    orderBy,
   });
 
   return (
@@ -26,13 +45,31 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => {
+              const isCurrentColumn = column.value === searchParams.orderBy;
+              const order = isCurrentColumn
+                ? searchParams.order === "desc"
+                  ? undefined
+                  : "desc"
+                : undefined;
+              return (
+                <Table.ColumnHeaderCell key={column.value}>
+                  <NextLink
+                    href={{
+                      query: { ...searchParams, orderBy: column.value, order },
+                    }}
+                  >
+                    {column.label}
+                  </NextLink>
+                  {isCurrentColumn &&
+                    (order ? (
+                      <ArrowUpIcon className="inline" />
+                    ) : (
+                      <ArrowDownIcon className="inline" />
+                    ))}
+                </Table.ColumnHeaderCell>
+              );
+            })}
           </Table.Row>
         </Table.Header>
         <Table.Body>
